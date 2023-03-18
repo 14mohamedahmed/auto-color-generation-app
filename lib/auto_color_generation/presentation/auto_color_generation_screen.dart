0af222bbd63ecc15932acd6eb6957fbcf37e93d1
Mohamed Ahmed <mohamed.ahmed@8worx.com>
2023-03-18 20:01:40 +0200

    handle periodic color genration, animated color switcher

--- lib/auto_color_generation/presentation/auto_color_generation_screen.dart
+++ lib/auto_color_generation/presentation/auto_color_generation_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:auto_generate_bg_color_app/auto_color_generation/data/datasource/color_generation_datasource.dart';
 import 'package:auto_generate_bg_color_app/auto_color_generation/data/repository/color_generation_repository.dart';
 import 'package:auto_generate_bg_color_app/auto_color_generation/domain/repository/base_color_generation_repository.dart';
@@ -15,30 +17,53 @@ class AutoColorGenerationScreen extends StatefulWidget {
 
 class _AutoColorGenerationScreenState extends State<AutoColorGenerationScreen> {
   Color appBgColor = Colors.white;
+  late Timer timer;
+  late GenerateRandomColorUseCase generateRodomColorUseCase;
   @override
   void initState() {
     super.initState();
+    BaseColorGenerationDataSource baseColorGenerationDataSource =
+        ColorGenerationDataSource();
+    BaseColorGenerationRepository baseColorGenerationRepository =
+        ColorGenerationRepository(baseColorGenerationDataSource);
+    generateRodomColorUseCase =
+        GenerateRandomColorUseCase(baseColorGenerationRepository);
+  }
+
+  _runColorGenerationInPeriodicTimer(
+      GenerateRandomColorUseCase generateRodomColorUseCase) {
+    setState(() {
+      appBgColor = generateRodomColorUseCase().randomColor;
+    });
+    timer = Timer.periodic(const Duration(seconds: 3), (timerState) {
+      if (timerState.isActive) {
+        setState(() {
+          appBgColor = generateRodomColorUseCase().randomColor;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    timer.cancel();
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        BaseColorGenerationDataSource baseColorGenerationDataSource =
-            ColorGenerationDataSource();
-        BaseColorGenerationRepository baseColorGenerationRepository =
-            ColorGenerationRepository(baseColorGenerationDataSource);
-        final useCase =
-            GenerateRandomColorUseCase(baseColorGenerationRepository);
         // we can use any state management insted of using statfult widget to avoid rebuild of entire  screen
-        setState(() {
-          appBgColor = useCase().randomColor;
-        });
+        _runColorGenerationInPeriodicTimer(generateRodomColorUseCase);
       },
       child: Scaffold(
-        backgroundColor: appBgColor,
-        body: const Center(
-          child: Text(AppStrings.generationColorScreenMessage),
+        body: AnimatedContainer(
+          duration: const Duration(milliseconds: 500),
+          color: appBgColor,
+          child: const Center(
+            child: Text(AppStrings.generationColorScreenMessage),
+          ),
         ),
       ),
     );
